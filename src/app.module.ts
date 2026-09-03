65600026c865c533260d51425eeb6b813f365acd
Nicolas Oris <nicolasoris98@gmail.com>
2026-09-02 23:15:59 -0300

    docker compose configurado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AsignaturasModule } from './modules/asignaturas/asignaturas.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AsignaturasModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        console.log('DB_HOST', configService.get<string>('DB_NAME'))
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          database: configService.get<string>('DB_NAME'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASS'),
+          port: configService.get<number>('DB_PORT'),
+          entities: [
+          __dirname + '/../**/*.entity{.ts,.js}',
+      ],
+          synchronize: true
+        }
+      }
+    }),
+    AsignaturasModule,
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
--- src/modules/asignaturas/entities/asignatura.entity.ts
+++ src/modules/asignaturas/entities/asignatura.entity.ts
@@ -1,1 +1,10 @@
-export class Asignatura {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('asignaturas')
+export class Asignatura {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    nombre: string
+}
